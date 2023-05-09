aacc7ca5bf29bc15fa1b4e74408ac2b807d8d109
Abdelrahman Farah Mohammed <abdelrahman.mohammed@bld.ai>
2023-05-09 17:20:37 +0300

    new full authentiction system

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import Login from './components/login/Login'
-import Register from './components/register/Register'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 import HomePage from './components/homepage/HomePage'
-import NavBar from './components/NavBar'
+
+import ActivateUserAccount from './components/auth/ActivateUserAccount'
+import ResetPassword from './components/auth/ResetPassword'
+import ConfirmResetPassword from './components/auth/ConfirmResetPassword'
+
+import Dashboard from './components/Dashboard'
 
 function App() {
 
   return (
     <>
-      <NavBar></NavBar>
-
       <div className="App">
-          <Routes>
-              <Route path="/" element={<HomePage/>} />
+        <Routes>
+          <Route index element={<HomePage />} />
 
-              <Route path="/login" element={<Login/>} />
-              <Route path="/register" element={<Register />} />
-          </Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/activate-user-account">
+            <Route path=":uid/:token" element={<ActivateUserAccount />} />
+          </Route>
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/confirm-password-reset">
+            <Route path=":uid/:token" element={<ConfirmResetPassword />} />
+          </Route>
+
+
+          <Route path="/dashboard" element={<Dashboard />}>
+          </Route>
+
+
+        </Routes>
       </div>
 
     </>
